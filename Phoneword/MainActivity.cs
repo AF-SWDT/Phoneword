587fa2915a7f923661e933608b8473a50601800e
AF-SWDT <33527887+AF-SWDT@users.noreply.github.com>
2020-06-09 19:51:53 -0700

    Mulltiscreen

--- Phoneword/MainActivity.cs
+++ Phoneword/MainActivity.cs
@@ -3,12 +3,22 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
+using System.Collections.Generic;
+using Android.Content;
+
+// This is a main screen (activity) of the Phoneword Android app
 
 namespace Phoneword
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity
+    //^ could be [Activity(Lable1 = "PhoneWord", MainLauncher = true)]
+
+    public class MainActivity : Activity
+        //^ previous version public class MainActivity : AppCompatActivity
     {
+        static readonly List<string> phoneNumbers = new List<string>();
+        //v OnCreate method, ets.
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -23,23 +33,48 @@ namespace Phoneword
             EditText phoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNumberText);
             TextView translatedPhoneWord = FindViewById<TextView>(Resource.Id.TranslatedPhoneword);
             Button translateButton = FindViewById<Button>(Resource.Id.TranslateButton);
+            Button translationHistoryButton = FindViewById<Button>(Resource.Id.TranslationHistoryButton);
 
-
-            //v Add code to translate number
+            //v Add method to translate number
+            string translatedNumber = string.Empty;
+            //^ could be omitted if we declare string translatedNumber bellow in method translateButton.Click += (sender, e) =>
             translateButton.Click += (sender, e) =>
             {
-                // Translate user's alphanumeric phone number to numeric
-                string translatedNumber = Core.PhonewordTranslator.ToNumber(phoneNumberText.Text);
+                //v Translate user's alphanumeric phone number to numeric
+
+                translatedNumber = Core.PhonewordTranslator.ToNumber(phoneNumberText.Text);
+                //^ could be string translatedNumber = Core.PhonewordTranslator.ToNumber(phoneNumberText.Text);
+                //^ if we do not declare string variable before method declaration translateButton.click += (sender, e) => 
+
                 if (string.IsNullOrWhiteSpace(translatedNumber))
                 {
-                    translatedPhoneWord.Text = string.Empty;
-                }
+                    translatedPhoneWord.Text = "";
+                    //^ could be translatedPhoneWord.Text = string.Empty;
+
+                }//^ if
+
                 else
                 {
+                    //v code for one MainActivity only
                     translatedPhoneWord.Text = translatedNumber;
-                }
+                    //v code for TranslationHistoryActivity
+                    phoneNumbers.Add(translatedNumber);
+                    //v code for TranslationHistoryActivity
+                    translationHistoryButton.Enabled = true;
+
+                }//^ else
 
             };//^ translateButton.Click += (sender, e) =>
+
+
+            //v Add method to wire up the Translation History button
+            translationHistoryButton.Click += (sender, e) =>
+            {
+                var intent = new Intent(this, typeof(TranslationHistoryActivity));
+                intent.PutStringArrayListExtra("phone_numbers", phoneNumbers);
+                StartActivity(intent);
+
+            };//^ translationHistoryButton.Click += (sender, e) =>
 
         }//^ protected override void OnCreate(Bundle savedInstanceState)
 
---
+++ Phoneword/TranslationHistoryActivity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Android.App;
+using Android.OS;
+using Android.Widget;
+
+// this is the second screen (activity) of the Phoneword Android app 
+
+namespace Phoneword
+{
+    [Activity(Label = "@string/translationHistory")]
+
+    public class TranslationHistoryActivity : ListActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            //v Create your application here
+            
+            
+            var phoneNumbers = Intent.Extras.GetStringArrayList("phone_numbers") ?? new string[0];
+            this.ListAdapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, phoneNumbers);
+
+        } //^ protected override void OnCreate(Bundle bundle)
+
+    } //^ public class TranslationHistoryActivity : ListActivity
+
+} //^ namespace Phoneword
